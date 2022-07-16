6ebd37a0e868d1748b4d54f3626641570aeb4931
rwellausen <108587248+rwellausen@users.noreply.github.com>
2022-07-16 15:15:18 -0300

    Adicionei arquivos via upload

---
+++ Hello-Github/src/br/com/ricardowellausen/git/HelloGit.java
@@ -0,0 +1,12 @@
+package br.com.ricardowellausen.git;
+
+//exemplo de integração do eclipse com o github
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+
+	}
+
+}
